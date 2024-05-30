07d277d41ba6c8d8c19892818fe6d09ebe8f1bc8
“[firstname <toobaanwer44@gmail.com>
2024-05-30 22:31:22 +0500

    push my mini calculator to github

---
+++ README.md
@@ -0,0 +1,1 @@
+"# cli-mini-calculator" 
---
+++ index.ts
@@ -0,0 +1,67 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+console.log("\nWelcome to our  Mini Calculator\n")
+let myLoop = true;
+while(myLoop){
+
+    let userInput = await inquirer.prompt([
+        {
+            type : "number",
+            name : "num1",
+            message : "Enter your first number"
+        },
+        {
+            type : "number",
+            name : "num2",
+            message : "Enter your second number"
+        },
+        {
+            type : "list",
+            name : "operator",
+            message : "Select the operator",
+            choices : ["Addition", "Subtraction", "Multiplication", "Division"]
+        }
+    ]);
+
+    let {num1, num2, operator} = userInput;
+
+    if(operator === "Addition") add();
+    if(operator === "Subtraction") sub();
+    if(operator === "Multiplication") multiply();
+    if(operator === "Division") divide();
+
+    function add(){
+         console.log(`\n${num1} + ${num2} = ${num1 + num2}`)
+    }
+
+    function sub(){
+        console.log(`\n${num1} - ${num2} = ${num1 - num2}`)
+    }
+
+    function multiply(){
+        console.log(`\n${num1} x ${num2} = ${num1 * num2}`)
+    }
+
+    function divide(){
+        console.log(`\n${num1} / ${num2} = ${num1 / num2}`)
+    }
+
+    // for more calculations
+
+    let calculateAgain = await inquirer.prompt({
+        type : "confirm",
+        name : "more",
+        message : "Do you want more calculations?",
+        default : false
+    });
+
+    if(!calculateAgain.more){
+        myLoop = false;
+        console.log("\nThank you for using Mini Calculator!")
+    }
+    
+}
+
+
